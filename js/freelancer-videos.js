8fd9be5d829f8c8824e1dc613c43b8d25e327f16
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2020-10-08 01:08:39 +0300

    Added freelancer tasks and practice.

--- js/freelancer-videos.js
+++ js/freelancer-videos.js
@@ -19,3 +19,21 @@ console.log(size);
 console.log("Freelancer. Video 4.");
 let userSize = "45" / "8";
 console.log(typeof userSize);
+
+console.log("Freelancer. Video 5.");
+
+// Variant 1
+console.log("35" + -"22"); // '35-22' - wrong
+// Variant 2
+console.log("35" * "22"); // 770 - correct
+// Variant 3
+// console.log("558" > 22++); // error
+// Variant 4
+let userCounter = 0;
+let newUsers = userCounter++;
+console.log(newUsers); //0 - wrong
+// Variant 5
+console.log((!false && 11) || (18 && !"")); // 11 - wrong
+// Variant 6
+let name = 0;
+console.log(name ?? "No name"); //0
