f2bd86aba8f79b0c7a14b4a5ac3c3dbeb87b5181
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-08-09 23:13:06 +0300

    Added simply weird solution.

--- random-problems/random.js
+++ random-problems/random.js
@@ -1,27 +1,23 @@
 // Unique pairs function
-const array = [1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4];
+const array = [1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4];
 
 function rearrangePairs(array) {
   const result = [];
-  const buffer = [];
-  let counter = 0;
-  let prevValue = null;
+  const uniqueValues = array.filter((v, i, a) => a.indexOf(v) === i);
+  const normalizedArr = uniqueValues.map(value =>
+    array.filter(el => el === value),
+  );
 
-  array.forEach(element => {
-    if (element !== prevValue) {
-      counter = 1;
-      result.push(element);
-    } else if (element === prevValue && !(counter > 1)) {
-      counter += 1;
-      result.push(element);
-    } else if (element === prevValue && counter > 1) {
-      buffer.push(element);
-    }
+  while (normalizedArr.some(subArr => subArr.length > 0)) {
+    normalizedArr.forEach(subArr => {
+      if (subArr.length >= 2) {
+        result.push(...subArr.splice(0, 2));
+      } else if (subArr.length === 1) {
+        result.push(...subArr.splice(0));
+      }
+    });
+  }
 
-    prevValue = element;
-    console.log('counter', counter);
-  });
-  console.log('buffer', buffer);
   return result;
 }
 
