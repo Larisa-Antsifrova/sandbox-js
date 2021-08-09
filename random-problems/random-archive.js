afe13ba63314cae676760fcaf26b98a88eb1b3dc
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-08-10 00:46:13 +0300

    Added new problems.

--- random-problems/random-archive.js
+++ random-problems/random-archive.js
@@ -37,3 +37,48 @@ function calculateMaxNestingDepth(htmlRef, currentLevel = 0) {
 }
 
 // console.log(calculateMaxNestingDepth(appRef));
+
+// Unique pairs function
+const array = [1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4];
+
+function rearrangePairs(array) {
+  const result = [];
+  const uniqueValues = array.filter((v, i, a) => a.indexOf(v) === i);
+  const normalizedArr = uniqueValues.map(value =>
+    array.filter(el => el === value),
+  );
+
+  while (normalizedArr.some(subArr => subArr.length > 0)) {
+    normalizedArr.forEach(subArr => {
+      if (subArr.length >= 2) {
+        result.push(...subArr.splice(0, 2));
+      } else if (subArr.length === 1) {
+        result.push(...subArr.splice(0));
+      }
+    });
+  }
+
+  return result;
+}
+
+// console.log(rearrangePairs(array));
+
+// Anagram detector
+function isAnagram(firstWord, secondWord) {
+  return (
+    firstWord.split('').sort().join('') === secondWord.split('').sort().join('')
+  );
+}
+
+// console.log(isAnagram('ANAGRAM', 'MGANRAA'));
+
+// Unique values
+const newArr = [1, 9, 0, 1, 5, 9, 1, 6];
+
+function leaveUnique(newArr) {
+  return newArr.reduce((acc, value) => {
+    return acc.includes(value) ? acc : [...acc, value];
+  }, []);
+}
+
+// leaveUnique(newArr);
