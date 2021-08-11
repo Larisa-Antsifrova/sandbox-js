74d0e13cad8d612968dbda73f1a88b219d4f5024
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-08-11 22:19:47 +0300

    Added solution.

--- codewars-sandbox/code-archive.js
+++ codewars-sandbox/code-archive.js
@@ -1,23 +1,23 @@
 // Archived solutions
 const recoverSecret = triplets => {
-  let tripletsCopy = triplets.slice(); 
-  let uniqueLetters = Array.from(new Set(triplets.flat())); 
-  const length = uniqueLetters.length; 
+  let tripletsCopy = triplets.slice();
+  let uniqueLetters = Array.from(new Set(triplets.flat()));
+  const length = uniqueLetters.length;
 
-  const result = []; 
+  const result = [];
 
   for (let i = 0; i < length; i++) {
     const nextLetter = uniqueLetters.find(letter =>
-      tripletsCopy.every(triplet => triplet.indexOf(letter) <= 0)
+      tripletsCopy.every(triplet => triplet.indexOf(letter) <= 0),
     );
     result.push(nextLetter);
     tripletsCopy = tripletsCopy.map(triplet =>
-      triplet.filter(letter => !result.includes(letter))
+      triplet.filter(letter => !result.includes(letter)),
     );
     uniqueLetters = uniqueLetters.filter(letter => letter !== nextLetter);
   }
 
-  return result.join(''); 
+  return result.join('');
 };
 
 const triplets1 = [
@@ -27,7 +27,18 @@ const triplets1 = [
   ['a', 't', 's'],
   ['h', 'a', 'p'],
   ['t', 'i', 's'],
-  ['w', 'h', 's']
+  ['w', 'h', 's'],
 ];
 
 console.log(recoverSecret(triplets1)); // Result: "whatisup"
+
+// Natural numbers
+function solution(number) {
+  const numbers = [...Array(Math.floor(number)).keys()];
+  return numbers.reduce(
+    (acc, value) => (!(value % 3) || !(value % 5) ? acc + value : acc),
+    0,
+  );
+}
+
+console.log(solution(10.5));
