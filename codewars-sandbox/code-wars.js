f7f5742c16e4bda30f188e2a26507b8c0f91a51d
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-08-15 21:05:18 +0300

    Added beginning of the solution.

--- codewars-sandbox/code-wars.js
+++ codewars-sandbox/code-wars.js
@@ -1,1 +1,16 @@
 // CodeWars. Next Kata
+var format = function (str, obj) {
+  const keys = Object.keys(obj);
+  let strCopy = str;
+
+  console.log('keys', keys);
+
+  keys.forEach(key => {
+    strCopy = strCopy.replace(`{${key}}`, `${obj[key]}`);
+  });
+
+  return strCopy;
+};
+
+var obj = { foo: 'Jack', bar: 'sandwich' };
+console.log(format('Hello {foo} - make me a {bar}', obj)); //"Hello Jack - make me a sandwich"
