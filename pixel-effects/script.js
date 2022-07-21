fefe42b80d70389a95d12c167b36de2c080f1734
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-07-21 21:40:39 +0300

    Disassebled image into pixel data.

--- pixel-effects/script.js
+++ pixel-effects/script.js
@@ -11,4 +11,18 @@ image1.addEventListener("load", drawImageOnCanvas);
 
 function drawImageOnCanvas() {
   ctx.drawImage(image1, 0, 0, canvas.width, canvas.height);
+  const scannedImage = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  for (let i = 0; i < scannedImage.data.length; i += 4) {
+    const total =
+      scannedImage.data[i] +
+      scannedImage.data[i + 1] +
+      scannedImage.data[i + 2];
+    const averageColorValue = total / 3;
+    scannedImage.data[i] = averageColorValue;
+    scannedImage.data[i + 1] = averageColorValue;
+    scannedImage.data[i + 2] = averageColorValue;
+  }
+
+  ctx.putImageData(scannedImage, 0, 0);
 }
