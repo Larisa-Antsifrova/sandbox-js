b80cb2ba1584b12e77120923fb5c79bcfc7f02d3
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-08-07 21:42:19 +0300

    Reached boilerplate.

--- pixel-effects/particles-text/script.js
+++ pixel-effects/particles-text/script.js
@@ -34,4 +34,49 @@ class Particle {
     this.baseY = this.y;
     this.desity = Math.random() * 30 + 1;
   }
+
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  update() {
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance < 100) {
+      this.size = 50;
+    } else {
+      this.size = 3;
+    }
+  }
 }
+
+function init() {
+  particlesArray = [];
+  for (let i = 0; i < 500; i++) {
+    let x = Math.random() * 500;
+    let y = Math.random() * 500;
+
+    particlesArray.push(new Particle(x, y));
+  }
+}
+
+init();
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].draw();
+    particlesArray[i].update();
+  }
+
+  requestAnimationFrame(animate);
+}
+
+animate();
